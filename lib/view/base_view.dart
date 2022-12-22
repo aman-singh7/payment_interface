f016ba270b16ef617423c52b97c6c04359b66b83
Aman <aman2@me.iitr.ac.in>
2022-12-23 03:13:24 +0530

    feat: initialize archiecture
    Signed-off-by: Aman <aman2@me.iitr.ac.in>

---
+++ lib/view/base_view.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../locator.dart';
+import '../viewmodels/base_viewmodel.dart';
+
+class BaseView<T extends BaseModel> extends StatefulWidget {
+  final Widget Function(BuildContext context, T model, Widget? child) builder;
+  final Function(T)? onModelReady;
+  final Function(T)? onModelDestroy;
+  final Function(Widget, T)? onDidUpdateWidget;
+  final Function(T)? onDidChangeDependencies;
+
+  const BaseView({
+    super.key,
+    required this.builder,
+    this.onModelReady,
+    this.onModelDestroy,
+    this.onDidUpdateWidget,
+    this.onDidChangeDependencies,
+  });
+
+  @override
+  _BaseViewState<T> createState() => _BaseViewState<T>();
+}
+
+class _BaseViewState<T extends BaseModel> extends State<BaseView<T>> {
+  T model = locator<T>();
+
+  @override
+  void initState() {
+    if (widget.onModelReady != null) {
+      widget.onModelReady!(model);
+    }
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (widget.onDidChangeDependencies != null) {
+      widget.onDidChangeDependencies!(model);
+    }
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(BaseView<T> oldWidget) {
+    if (widget.onDidUpdateWidget != null) {
+      widget.onDidUpdateWidget!(oldWidget, model);
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    if (widget.onModelDestroy != null) {
+      widget.onModelDestroy!(model);
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<T>(
+      create: (context) => model,
+      child: Consumer<T>(
+        builder: widget.builder,
+      ),
+    );
+  }
+}
---
+++ lib/viewmodels/base_viewmodel.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../enums/viewstate.dart';
+
+class BaseModel extends ChangeNotifier {
+  ViewState _state = ViewState.idle;
+
+  ViewState get state => _state;
+
+  void setState(ViewState viewState) {
+    _state = viewState;
+    notifyListeners();
+  }
+
+  String _errorMessage = '';
+
+  String get errorMessage => _errorMessage;
+
+  void setErrorMessage(String errorMessage) {
+    _errorMessage = errorMessage;
+    notifyListeners();
+  }
+}
